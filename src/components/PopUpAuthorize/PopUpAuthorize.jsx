34bd8f0922ca329737cc590165ee440eefe15331
mihai807 <mihai807@mail.ru>
2021-12-03 23:09:51 +0300

    add rotate poup

--- src/components/PopUpAuthorize/PopUpAuthorize.jsx
+++ src/components/PopUpAuthorize/PopUpAuthorize.jsx
@@ -6,7 +6,7 @@ function PopUpAuthorize({ handleToggle }) {
   return (
     <div className="popup">
       <div className="overlay" onClick={handleToggle} />
-      <div className="popup__wrap">
+      <div className="popup__wrap auth-wrap">
         <span className="popup__title">Войдите в учетную запись</span>
         <form action="" className="popup__form">
           <input type="text" className="popup__input" placeholder="Имя" />
