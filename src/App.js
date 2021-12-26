51c62e07b0872641a23c2df4b8aa6c452d801a42
mihai807 <mihai807@mail.ru>
2021-12-26 21:13:27 +0300

    add private routes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import BookmarkPosts from "./components/BookmarkPosts/BookmarkPosts";
 import Main from "./pages/Main/Main";
 import Subscribes from "./components/Subscribes/Subscribes";
 import Profile from "./components/Profile/Profile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const curUser = useSelector((state) => state.user.curUser);
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route element={<Layout />}>
-          <Route path="Home" element={<Posts />} />
-          <Route path="Bookmarks" element={<BookmarkPosts />} />
-          <Route path="Subscribes" element={<Subscribes />} />
-          <Route path="Profile" element={<Profile />} />
-        </Route>
+        {Object.keys(curUser).length === 0 && (
+          <Route path="/" element={<Main />} />
+        )}
+        {Object.keys(curUser).length !== 0 && (
+          <Route element={<Layout />}>
+            <Route path="Home" element={<Posts />} />
+            <Route path="Bookmarks" element={<BookmarkPosts />} />
+            <Route path="Subscribes" element={<Subscribes />} />
+            <Route path="Profile" element={<Profile />} />
+          </Route>
+        )}
       </Routes>
     </div>
   );
